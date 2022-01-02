60d3bf55fee8f615519b03134ee7706a1752a9e6
jayathungek <jayathunge.work@gmail.com>
2022-01-02 11:46:13 +0000

    feature: add is_production method

--- folderbot/secret/__init__.py
+++ folderbot/secret/__init__.py
@@ -5,7 +5,8 @@ import json
 def get_discord_token() -> str:
     token = os.getenv("DISCORD_TOKEN", None)
     if not token:
-        with open("secret/discord.json", "r") as fh:
+        secret_path = f"{os.path.dirname(os.path.realpath(__file__))}/discord.json"
+        with open(secret_path, "r") as fh:
             discord_json = json.load(fh)
             token = discord_json["DISCORD_TOKEN"]
 
@@ -14,3 +15,7 @@ def get_discord_token() -> str:
 
 def get_redis_url() -> str:
     return os.getenv('REDISTOGO_URL', 'redis://localhost:7777')
+
+
+def is_production() -> bool:
+    return os.getenv("DISCORD_TOKEN", None) is not None
